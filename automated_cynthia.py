324bb7d3dbd98242324e828b7e9881ab1fdc0cd1
Jason Huang <Jason.Isaac.Huang@gmail.com>
2017-12-18 17:22:35 +1100

    Set up basic CLI.

--- automated_cynthia.py
+++ automated_cynthia.py
@@ -1,13 +1,29 @@
 from scrapers.ratecity import RateCity
 from pprint import pprint
+import argparse
 import sys
 
 
 def main(argv):
-    ratecity = RateCity()
-    # pprint(ratecity.lenders())
-    ratecity.to_csv(ratecity.products(), 'ratecity.csv')
-    # pprint(ratecity.products())
+    parser = argparse.ArgumentParser(description='Scrapes various websites for mortgages')
+    parser.add_argument('-q', '--quiet', help='suppress all output', action='store_true')
+    parser.add_argument('-s', '--source', help='choose which sources to scrape from', nargs='+')
+    args = parser.parse_args()
+
+    if args.quiet:
+        print('suppressing all output')
+        print('TODO')
+    if args.source:
+        for source in set(args.source):
+            if source.lower() == 'ratecity':
+                print('Scraping from RateCity')
+                ratecity = RateCity()
+                ratecity.to_csv(ratecity.products(), 'ratecity.csv')
+            else:
+                print(source + ' not a valid source, skipping...')
+
+    else:
+        print("no source specified, scraping from all sources")
 
 if __name__ == "__main__":
     main(sys.argv)
